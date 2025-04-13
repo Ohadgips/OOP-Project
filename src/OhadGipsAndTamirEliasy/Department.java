b018e8af567f42e218a6b10756968bd8b113022c
Ohadgips <138259832+Ohadgips@users.noreply.github.com>
2025-04-13 22:38:02 +0300

    Department And Committee Classes and ControlPanel Base

--- src/OhadGipsAndTamirEliasy/Department.java
+++ src/OhadGipsAndTamirEliasy/Department.java
@@ -6,11 +6,25 @@ public class Department {
     Lecturer[] lecturers;
     int lecturersSize;
 
+    public void resizeLecturers() {
+        if (lecturersSize >= lecturers.length) {
+            Lecturer[] temp = new Lecturer[lecturersSize * 2];
+            System.arraycopy(lecturers, 0, temp, 0, lecturersSize);
+            lecturers = temp;
+        }
+    }
+
     public Department(String name, int numOfStudents) {
         setName(name);
         setNumOfStudents(numOfStudents);
+        lecturers = new Lecturer[1];
+        lecturersSize = 0;
     }
-
+    public boolean addLecturer(Lecturer lecturer){
+        resizeLecturers();
+        lecturers[lecturersSize++] = lecturer;
+        return true;
+    }
     public String getName() {
         return this.name;
     }
