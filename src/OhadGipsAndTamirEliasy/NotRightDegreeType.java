fb525a8e1af56bf65b18b1806c0e455d0e284f26
Ohadgips <ohadgips@yahoo.com>
2026-06-26 09:46:43 +0300

    SOLID: D Principle

--- src/OhadGipsAndTamirEliasy/NotRightDegreeType.java
+++ src/OhadGipsAndTamirEliasy/NotRightDegreeType.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 // Submitted By: Tamir Eliasy 216430298 & Ohad Gips 215426883
 public class NotRightDegreeType extends Exception implements Serializable {
     public NotRightDegreeType() {
-      String str = "Lecturer degree type is not as committee degree type";
-      super(str);
+      super("Lecturer degree type is not as committee degree type");
     }
 }
